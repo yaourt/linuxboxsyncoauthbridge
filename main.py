5d9388ec82ad4f9d60e72b073634939d0ae36f63
Yaourt <yaourt@yaourtprod.net>
2013-12-09 07:22:48 +0100

    WIP

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from google.appengine.api import urlfetch
 from google.appengine.api import memcache
 import urllib
 import uuid
-import Crypto.Random
 import yaml
 
 # Declaring Bottle application,
@@ -20,7 +19,7 @@ bottle.debug(True)
 config = yaml.load(open('oauth.yaml', 'rb'))
 client_id = config['client_id']
 client_secret = config['client_secret']
-redirect_uri = 'http://127.0.0.1:8080/code'
+redirect_uri = 'https://linuxboxsyncbridge.appspot.com/authorize'
 
 html= """
 <html>
@@ -40,7 +39,7 @@ def root():
 
     redirect(url, 302)
 
-@app.route('/code')
+@app.route('/authorize')
 def code():
     code = request.query.code
     state = request.query.state
@@ -52,11 +51,11 @@ def code():
     url = "https://www.box.com/api/oauth2/token"
 
     params = {
-        'grant_type' : 'authorization_code',
-        'code' : code,
-        'client_id' : client_id,
-        'client_secret' : client_secret,
-        'redirect_uri' : redirect_uri,
+        'grant_type': 'authorization_code',
+        'code': code,
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'redirect_uri': redirect_uri,
     }
     payload = urllib.urlencode(params)
 
@@ -68,10 +67,28 @@ def code():
     )
     if 200 == result.status_code:
         memcache.add(state, result.content, 60)
-    #return redirect("oauth:%s" % (state))
     return html % (urllib.quote(result.content))
 
-@app.route('/token')
-def token():
-    code = request.query.state
-    return code
+@app.route(path='/refresh', method='POST')
+def refresh():
+    json_data = request.json
+    refresh_token = json_data['refresh_token']
+
+    url = "https://www.box.com/api/oauth2/token"
+
+    params = {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token,
+        'client_id': client_id,
+        'client_secret': client_secret,
+    }
+    payload = urllib.urlencode(params)
+
+    result = urlfetch.fetch(
+        url=url,
+        payload=payload,
+        method=urlfetch.POST,
+        deadline=15
+    )
+    if 200 == result.status_code:
+        return result.content
